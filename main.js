642dea6956947591ce4716a29fa9d010b8997031
Nikolas <niksprunt@gmail.com>
2019-03-06 20:02:56 -0700

    fixed layout

--- main.js
+++ main.js
@@ -9,18 +9,15 @@ films.forEach( (film) => {
     let tile = document.createElement('div')
     
 let titleElement = document.createElement('h1')
-intro.appendChild(titleElement)
+tile.appendChild(titleElement)
 titleElement.textContent = film.title
 
 let crawlElement = document.createElement('h3')
-intro.appendChild(crawlElement)
+tile.appendChild(crawlElement)
 crawlElement.textContent = film.opening_crawl
 
-let lineBreak = document.createElement('hr')
-intro.appendChild(lineBreak)
-})
-
 intro.appendChild(tile)
+})
 
 const men = people. filter(person => person.gender === "male")
 const women = people. filter(person => person.gender === "female")
