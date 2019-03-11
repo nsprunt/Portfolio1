aed45c20a21e6c40c7af94cc6f47ae8814e2b643
Nikolas <niksprunt@gmail.com>
2019-03-10 22:36:55 -0600

    working on people page. not showing up yet.

--- scripts/strwarsppl.js
+++ scripts/strwarsppl.js
@@ -1,11 +1,33 @@
 import { people } from '../data/people.js'
+import { planets } from '../data/planets.js'
+
+const getLastNumber = url => {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if(url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
+
+const allHomeWorlds = people.map(person => {
+    let foundWorld = planets.find(planet => {
+        return planet.url === person.homeworld
+    })
+    return {
+        name: person.name,
+        home: foundWorld.name
+    }
+}) 
+
+
 
 const men = people. filter(person => person.gender === "male")
 const women = people. filter(person => person.gender === "female")
 const other = people. filter(person => (person.gender === "n/a") ||
      (person.gender === "none") || (person.gender === "hermaphrodite"))
     
-console.log(men, women, other)
+ console.log(men, women, other)
 
 const main = document.querySelector('.container')
 
