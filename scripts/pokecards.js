ad17a584ed67f6372e39dcd735fc83f257ec009f
Nikolas <niksprunt@gmail.com>
2019-04-03 20:28:37 -0600

    added images and some styling. Mr. Mime image is not working...

--- scripts/pokecards.js
+++ scripts/pokecards.js
@@ -1,42 +1,36 @@
 import { pokemon } from '../data/pokemon.js'
 
+console.log(pokemon)
+
+const mainContainer = document.querySelector('.container')
+
+function createPokeCard(pokeData) {
+    console.log(pokeData.id)
+    let card = document.createElement('div')
+    card.className = 'box'
+    let figure = document.createElement('figure')
+    let caption = document.createElement('figcaption')
+    let image = document.createElement('img')
+
+    let upperName = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)
+    caption.textContent = upperName
+    image.src = `../images/${pokeData.id}${upperName}.png`
+    figure.appendChild(image)
+    figure.appendChild(caption)
+    card.appendChild(figure)
+    mainContainer.appendChild(card)
+}
+
 pokemon.forEach((singleMon) => {
     fetch(singleMon.url)
     .then(function(response) {
         return response.json();
     })
     .then(function(myJson) {
-        console.log(myJson)
-        //createPokeCard(myJson)
+        //console.log(myJson)
+        createPokeCard(myJson)
     })
 })
-
-/*function matchIdToImage(aPokemon) {
-    if (aPokemon.id < 10) {
-        aPokemon.imageID = "00" + aPokemon.id
-    }
-    if(aPokemon.id > 9 && aPokemon.id < 100 ) {
-        aPokemon.imageID = aPokemon.id
-    }
-    aPokemon.name = aPokemon.name.charAt(0).toUpperCase() + aPokemon.slice(1)
-    return aPokemon
-}*/
-
-console.log(pokemon)
-
-const mainContainer = document.querySelector('.container')
-
-function createPokeCard(pokedata) {
-    let card = document.createElement('div')
-    let name = document.createElement('p')
-    let image = document.createElement('img')
-
-    name.textContent = pokedata.name
-    image.src = pokedata.sprites.front_default
-    card.appendChild(name)
-    card.appendChild(image)
-    mainContainer.appendChild(card)
-}
 /*class Pokemon {
     constructor(id, name) {
         this.id = id,
@@ -79,3 +73,27 @@ function createPokeCard(pokedata) {
 
 createPokeCard(newPokemon) */
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function matchIdToImage(aPokemon) {
+    if (aPokemon.id < 10) {
+        aPokemon.imageID = "00" + aPokemon.id
+    }
+    if(aPokemon.id > 9 && aPokemon.id < 100 ) {
+        aPokemon.imageID = aPokemon.id
+    }
+    aPokemon.name = aPokemon.name.charAt(0).toUpperCase() + aPokemon.slice(1)
+    return aPokemon
+}*/
