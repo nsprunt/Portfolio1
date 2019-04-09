1fa7e307f345f1fb7056ea0500f8d56e8d67aa2d
Nikolas <niksprunt@gmail.com>
2019-04-09 11:09:51 -0600

    got pokeball centered but moves text is not.

--- scripts/pokecards.js
+++ scripts/pokecards.js
@@ -1,12 +1,5 @@
 import { pokemon } from '../data/pokemon.js'
 
-class Pokemon {
-    constructor(id, name) {
-        this.id = 0,
-        this.name = name
-    }
-}
-
 const mainContainer = document.querySelector('.container')
 
 function cardFront(pokeData) {
@@ -16,13 +9,12 @@ function cardFront(pokeData) {
     let caption = document.createElement('figcaption')
     let image = document.createElement('img')
 
-    //let upperName = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)
     caption.textContent = pokeData.name
-if(pokeData.id !== 0) {
-    image.src = `../images/${pokeData.id}${pokeData.name}.png`
-} else {
-    image.src = `../images/pokeball.png`
-}
+    if (pokeData.id !== 0) {
+        image.src = `../images/${pokeData.id}${pokeData.name}.png`
+    } else {
+        image.src = `../images/pokeball.png`
+    }
 
     figure.appendChild(image)
     figure.appendChild(caption)
@@ -32,10 +24,12 @@ if(pokeData.id !== 0) {
 
 function cardBackInfo(pokeData) {
     let infoDiv = document.createElement('div')
+    infoDiv.className = 'infoDiv'
     let moveOne = document.createElement('p')
     let moveTwo = document.createElement('p')
     let moveThree = document.createElement('p')
     let moveFour = document.createElement('p')
+    console.log(pokeData)
     moveOne.textContent = pokeData.moves[0].move.name
     moveTwo.textContent = pokeData.moves[1].move.name
     moveThree.textContent = pokeData.moves[2].move.name
@@ -63,9 +57,9 @@ function createPokeCard(pokeData) {
     scene.className = 'scene'
     let card = document.createElement('div')
     card.className = 'card'
-    
-    card.addEventListener( 'click', function() {
-      card.classList.toggle('is-flipped');
+
+    card.addEventListener('click', function () {
+        card.classList.toggle('is-flipped');
     });
 
     card.appendChild(cardFront(pokeData))
@@ -76,80 +70,50 @@ function createPokeCard(pokeData) {
 
 pokemon.forEach((singleMon) => {
     fetch(singleMon.url)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(myJson) {
-        createPokeCard(myJson)
-    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (myJson) {
+            createPokeCard(myJson)
+        })
 })
 
+class Pokemon {
+    constructor(id, name) {
+        this.id = 0,
+            this.name = name
+        this.moves = [
+            {
+                move: {
+                    name: "smash",
+                }
+            },
+            {
+                move: {
+                    name: "obliterate",
+                }
+            },
+            {
+                move: {
+                    name: "implode",
+                }
+            },
+            {
+                move: {
+                    name: "chop",
+                }
+            }
+
+        ]
+    }
+}
 
 const newPokemonButton = document.querySelector('button')
 
-newPokemonButton.addEventListener('click', function() {
+newPokemonButton.addEventListener('click', function () {
     let newPokeName = prompt('Enter the name of your new pokemon')
-    createPokeCard(new Pokemon(80,newPokeName))
+    createPokeCard(new Pokemon(80, newPokeName))
 });
 
 
-/*
-let newPokemon = new Pokemon(80, 'Thoremon')
 
-console.log(newPokemon)
-
-pokemon.forEach((singleMon) => {
-    fetch(singleMon.url)
-    .then(function(response) {
-        return response.json ()
-    })
-.then(function(myJson) {
-    console.log(myJson);
-    createPokeCard(myJson)
-    })
-});
-
-console.log(pokemon)
-
-const mainContainer = document.querySelector('.container')
-
-function createPokeCard(pokedata) {
-    let card = document.createElement('div')
-    let figure = document.createElement('figure')
-    //card.className = 'box'
-    let caption = document.createElement('figcaption')
-    let image = document.createElement('img')
-
-    name.textContent = pokedata.namecard.appendChild(name)
-    image.src = pokedata.sprites.front_default
-    card.appendChild(name)
-    card.appendChild(image)
-    mainContainer.appendChild(card)
-}
-
-createPokeCard(newPokemon) */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function matchIdToImage(aPokemon) {
-    if (aPokemon.id < 10) {
-        aPokemon.imageID = "00" + aPokemon.id
-    }
-    if(aPokemon.id > 9 && aPokemon.id < 100 ) {
-        aPokemon.imageID = aPokemon.id
-    }
-    aPokemon.name = aPokemon.name.charAt(0).toUpperCase() + aPokemon.slice(1)
-    return aPokemon
-}*/
